cea7869e5f98e7f40ac88c4ba30af67c6b0e4b06
Pascal Schwabe <pascalschwabe@yahoo.de>
2024-01-19 07:12:27 +0100

    Remove @PWA annotation from Project, because of build problem

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -3,7 +3,6 @@ package com.example.application;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
-import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.server.ServiceInitEvent;
 import com.vaadin.flow.server.VaadinServiceInitListener;
 import com.vaadin.flow.theme.Theme;
@@ -21,7 +20,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @Theme(value = "collaboration")
-@PWA(name = "GamePal", shortName = "GamePal", offlineResources = {})
+//@PWA(name = "GamePal", shortName = "GamePal", offlineResources = {})
 @Push
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
